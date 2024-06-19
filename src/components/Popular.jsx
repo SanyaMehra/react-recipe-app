e54e9f400f0f0b7ef77e57194a58925760179042
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2024-06-19 13:06:20 -0400

    first commit

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -33,7 +33,7 @@ function Popular() {
     return (
         <div>
             <Wrapper>
-                <h3>Popular Picks</h3>
+                <h3>Fan Favorites</h3>
                     <Splide
                         options = {{
                             perPage: 4,
